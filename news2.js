8eb9877678cc94d3be042fd02b6d40618c43e7c7
anshuman <anshumannarendrajitsingh@gmail.com>
2022-08-03 02:02:28 +0530

    news website

---
+++ news2.js
@@ -0,0 +1,26 @@
+let obj=JSON.parse(sessionStorage.getItem("info"));
+console.log(obj);
+let title=document.getElementById("title");
+title.innerText=obj.title;
+let author=document.getElementById("author");
+author.innerText=`Author : ${obj.author}`;
+let time=document.getElementById("time");
+time.innerText=`Published At : ${new Date(obj.publishedAt)}`;
+let ori=document.getElementById("origin");
+let a=document.createElement("a");
+a.innerText="Read the original and full article here";
+a.href=obj.url;
+ori.append(a);
+let img=document.getElementById("articlePic");
+img.innerHTML=`<img src="${obj.urlToImage} " alt="">`;
+let story=document.getElementById("articleStory");
+let txt="";
+let str=obj.content.split("");
+// console.log(str.length);
+// str.content.foreach(function(elem){
+//     txt=txt+elem;
+// });
+// for (let i=0;i<str.length;i++){
+//     txt=txt+str[i];
+// }
+// console.log(txt);
